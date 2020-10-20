be28b62e845fa0e3fb42e62fcbf3da277346d23a
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-20 18:19:20 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,12 @@
 # Write your solution here
+
+current_time = Time.now
+converted_time = current_time.to_i
+
+if converted_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+# converted_time % 2 == 0 ? "Even!" : "Odd!"
