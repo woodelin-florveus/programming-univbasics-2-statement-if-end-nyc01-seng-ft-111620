f4c43cc8688d3d277a0b1056397f92c9b47b4541
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-20 18:46:30 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -3,10 +3,20 @@
 current_time = Time.now
 converted_time = current_time.to_i
 
-if converted_time % 2 == 0
-  puts "Even!"
-else
+# if converted_time % 2 == 0
+#   puts "Even!"
+# else
+#   puts "Odd!"
+# end
+
+
+
+case  converted_time
+when converted_time % 2 == 0
+  puts puts "Even!"
+when converted_time % 2 != 0
   puts "Odd!"
 end
+
 
 # converted_time % 2 == 0 ? "Even!" : "Odd!"
